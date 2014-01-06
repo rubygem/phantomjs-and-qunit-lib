4f411f7fe4c5ef94a1d36fb3c7cbe5ed4d58844c
ruby_gem <gemma.cameron@gmail.com>
2014-01-06 14:37:29 +0000

    got a test which triggers and checks that the option has been removed

--- tests/cannotSelectSameThingInTwoDropDowns/tests.js
+++ tests/cannotSelectSameThingInTwoDropDowns/tests.js
@@ -1,3 +1,21 @@
-test( "hello test", function() {
-	ok( 1 == "1", "Is 1 equal to 1?" );
+test( "When data centre is selected in a end list, it is removed from b end list ", function() {
+	var aEndOptions = $('#theOptions1').text();
+	var bEndOptions = $('#theOptions2').text();
+	assertContains(bEndOptions, 'a');
+	assertContains(bEndOptions, 'b');
+	assertContains(bEndOptions, 'c');
+
+	$('#theOptions1').val('2').trigger('change');
+
+	assertContains($('#theOptions2').text(), 'a');
+	assertContains($('#theOptions2').text(), 'b');
+	assertDoesNotContain($('#theOptions2').text(), 'c');
 });
+
+function assertContains(text, subString){
+	ok((text.indexOf(subString) > -1), "Whoops! " + text + " does not contain " +subString);
+}
+
+function assertDoesNotContain(text, subString){
+	notEqual((text.indexOf(subString) > -1), true, "Bugger! " + text + " does contain " +subString);
+}
