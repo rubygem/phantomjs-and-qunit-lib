e33e5b427a7b17caba256d8870e88d8fb1228bf6
ruby_gem <gemma.cameron@gmail.com>
2014-01-06 14:42:42 +0000

    refactoring

--- tests/cannotSelectSameThingInTwoDropDowns/tests.js
+++ tests/cannotSelectSameThingInTwoDropDowns/tests.js
@@ -1,15 +1,23 @@
 test( "When data centre is selected in a end list, it is removed from b end list ", function() {
+	//Get the values of the selects
 	var aEndOptions = $('#aEnd').text();
 	var bEndOptions = $('#bEnd').text();
+	
+	//Check everything is there
 	assertContains(bEndOptions, 'a');
 	assertContains(bEndOptions, 'b');
 	assertContains(bEndOptions, 'c');
 
+	//Select one of the options
 	$('#aEnd').val('2').trigger('change');
 
-	assertContains($('#bEnd').text(), 'a');
-	assertContains($('#bEnd').text(), 'b');
-	assertDoesNotContain($('#bEnd').text(), 'c');
+	//Get the updated options
+	bEndOptions = $('#bEnd').text();
+
+	//Check the option has been removed
+	assertContains(bEndOptions, 'a');
+	assertContains(bEndOptions, 'b');
+	assertDoesNotContain(bEndOptions, 'c');
 });
 
 function assertContains(text, subString){
